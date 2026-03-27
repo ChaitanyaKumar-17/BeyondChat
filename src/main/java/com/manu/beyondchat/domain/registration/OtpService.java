2271c07f93ca1e988ecce68a2c60ba736758d2c7
Chaitanya Kumar <chaitanya0817@gmail.com>
2026-03-27 23:21:32 +0530

    some refining

--- src/main/java/com/manu/beyondchat/domain/registration/OtpService.java
+++ src/main/java/com/manu/beyondchat/domain/registration/OtpService.java
@@ -10,7 +10,7 @@ import java.time.Instant;
 public class OtpService {
     public String generateSecureOtp() {
         SecureRandom secureRandom = new SecureRandom();
-        // Generates a random number between 100000 and 999999
+        // Generates a random number between 100000 and 999999 (for 6 digit otp)
         int otp = 100000 + secureRandom.nextInt(900000);
         return String.valueOf(otp);
     }
